5cea6e5b588980b37bc661a6eb8dfe34a9f0af7c
linkdom <donglinkang2021@163.com>
2025-11-16 03:42:37 +0000

    [exp]test the model with flash_attention_2

--- scripts/run_sft_test.sh
+++ scripts/run_sft_test.sh
@@ -9,4 +9,5 @@ export HF_HUB_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 
-uv run cs336_alignment/train_sft.py
+# uv run cs336_alignment/train_sft.py
+uv run cs336_alignment/train_sft.py --config-name exp_sft_fa2_omr12k
